0cc3a396789997b74d20d476164c6108b15d84ef
David Guess <david.guess@gmail.com>
2019-08-04 16:34:48 +1000

    Add token name and symbol

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,14 +14,15 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string public constant name = "DG Token";
+    string public constant symbol = "XDG";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-    
+
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
